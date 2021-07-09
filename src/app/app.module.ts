b5492b857c420a4dabf6372976ee625426d8cdd9
camilo19930 <edwinbp20@gmail.com>
2021-07-09 17:49:34 -0500

    Creación de componente de navegación

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './routes/app-routing.module';
 import { AppComponent } from './app.component';
 import { NotfoundComponent } from './components/notfound/notfound.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NotfoundComponent
+    NotfoundComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
