e3c1768290dcee3a2267d78eea3f8da0a2bd32f3
camilo19930 <edwinbp20@gmail.com>
2021-07-22 10:18:23 -0500

    ceration of aplication style

--- src/app/components/product/add-product/add-product.component.ts
+++ src/app/components/product/add-product/add-product.component.ts
@@ -1,7 +1,10 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
-import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { ProductService } from 'src/app/services/product.service';
+import Swal from 'sweetalert2';
+
+
 @Component({
   selector: 'app-add-product',
   templateUrl: './add-product.component.html',
@@ -14,12 +17,16 @@ export class AddProductComponent implements OnInit {
   constructor(
     private formBuilder: FormBuilder,
     private productService: ProductService,
-    @Inject(MAT_DIALOG_DATA) public data
+    @Inject(MAT_DIALOG_DATA) public data,
+    private dialogRef: MatDialogRef<AddProductComponent>
   ) {
     this.form = this.construirFormulario();
   }
   ngOnInit(): void {
     console.log(this.data);
+    if (this.data.data.accion === 'editar') {
+      this.llenarCampos(this.data.data.registros[0]);
+    }
   }
 
   private construirFormulario(): any {
@@ -38,27 +45,60 @@ export class AddProductComponent implements OnInit {
   get cant(): any { return this.form.get('cant'); }
   get fech_update(): any { return this.form.get('fech_update'); }
 
-  async Guardar(): Promise<any> {
-    const data = {
-      name: this.name.value,
-      description: this.description.value,
-      reference: +this.reference.value,
-      cant: +this.cant.value,
-      fech_update: '2021/07/19'
-    };
-    try {
-      const res = await this.productService.createProduct(data);
-      console.log(res);
-    } catch (error) {
-      console.log(error);
+  async guardar(): Promise<any> {
+    if (!this.form.invalid) {
+      const data = {
+        name: this.name.value,
+        description: this.description.value,
+        reference: +this.reference.value,
+        cant: +this.cant.value,
+        fech_update: '2021/07/19'
+      };
+      try {
+        const res = await this.productService.createProduct(data);
+        console.log(res);
+        await this.tinyAlert();
+        this.dialogRef.close(true);
+      } catch (error) {
+        this.tinyAlertError();
+        console.log(error);
+      }
+    } else {
+      Swal.fire({
+        position: 'top-end',
+        icon: 'warning',
+        title: 'Debe llenar los campos obligatorios',
+        showConfirmButton: false,
+        timer: 3000,
+        heightAuto: false,
+        width: 300
+      });
     }
   }
-  tinyAlert(): void{
-    // Swal.fire({
-    //   title: 'Error!',
-    //   text: 'Do you want to continue',
-    //   icon: 'error',
-    //   confirmButtonText: 'Cool'
-    // });
+
+  llenarCampos(datos): void{
+    this.name.setValue(datos.name);
+    this.description.setValue(datos.description);
+    this.reference.setValue(datos.reference);
+    this.cant.setValue(datos.cant);
+    this.fech_update.setValue(datos.fech_update);
+  }
+
+
+  tinyAlert(): void {
+    Swal.fire({
+      title: 'Exitoso!',
+      text: 'El registro ha sido creado',
+      icon: 'success',
+      confirmButtonText: 'Aceptar'
+    });
+  }
+  tinyAlertError(): void {
+    Swal.fire({
+      title: 'Exitoso!',
+      text: 'El registro ha sido creado',
+      icon: 'error',
+      confirmButtonText: 'Aceptar'
+    });
   }
 }
--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,31 +1,11 @@
 import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { MatDialog } from '@angular/material/dialog';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatTableDataSource } from '@angular/material/table';
 import { ProductService } from 'src/app/services/product.service';
 import { AddProductComponent } from './add-product/add-product.component';
 import { ProductInterface } from './product-interface';
-
-export interface PeriodicElement {
-  name: string;
-  codigo: number;
-  descripcion: number;
-  symbol: string;
-}
-
-const ELEMENT_DATA: PeriodicElement[] = [
-  { codigo: 1, name: 'Hydrogen', descripcion: 1.0079, symbol: 'H' },
-  { codigo: 2, name: 'Helium', descripcion: 4.0026, symbol: 'He' },
-  { codigo: 3, name: 'Lithium', descripcion: 6.941, symbol: 'Li' },
-  { codigo: 4, name: 'Beryllium', descripcion: 9.0122, symbol: 'Be' },
-  { codigo: 5, name: 'Boron', descripcion: 10.811, symbol: 'B' },
-  { codigo: 6, name: 'Carbon', descripcion: 12.0107, symbol: 'C' },
-  { codigo: 7, name: 'Nitrogen', descripcion: 14.0067, symbol: 'N' },
-  { codigo: 8, name: 'Oxygen', descripcion: 15.9994, symbol: 'O' },
-  { codigo: 9, name: 'Fluorine', descripcion: 18.9984, symbol: 'F' },
-  { codigo: 10, name: 'Neon', descripcion: 20.1797, symbol: 'Ne' },
-];
-
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
@@ -33,45 +13,51 @@ const ELEMENT_DATA: PeriodicElement[] = [
 })
 export class ProductComponent implements AfterViewInit {
   displayedColumns: string[] = ['id', 'name', 'description', 'reference', 'cant', 'fech_update', 'select'];
-  // dataSource: any = new MatTableDataSource<PeriodicElement>(ELEMENT_DATA);
   @ViewChild(MatPaginator) paginator: MatPaginator;
   dataSource: any;
-  checked = false;
-  indeterminate = false;
+  form: any = FormGroup;
+  dataEnviar = [];
 
   constructor(
+    private formBuilder: FormBuilder,
     private productService: ProductService,
     public dialog: MatDialog) {
     this.obtenerProductos();
   }
+  ngAfterViewInit(): void {}
 
-  ngOnit(): void {
-  }
-
-  ngAfterViewInit(): void {
-    // this.dataSource.paginator = this.paginator;
-  }
   async obtenerProductos(): Promise<any> {
     try {
       const res = await this.productService.listAllProducts();
-      console.log(res);
-      // return res;
       this.dataSource = new MatTableDataSource<ProductInterface>(res);
     } catch (error) {
       console.log(error);
     }
   }
-  async agregar(): Promise<any> {
+  agregar(): void {
+    this.llamarPopup('agregar');
+  }
+  editar(): void {
+    this.llamarPopup('editar', this.dataEnviar);
+  }
+  seleccionarCampo(event: any, elemento: any): void {
+    console.log(elemento);
+    this.dataEnviar.push(elemento);
+  }
+
+  llamarPopup(accionEnviar: string, data?: {}): void {
     const dialogRef = this.dialog.open(AddProductComponent, {
       panelClass: 'custom-dialog-container',
-      height: '200px',
+      height: '170px',
       width: '50%',
       data: {
         data: {
-          name: 'Camilo'
+          accion: accionEnviar,
+          registros: data === undefined ? null : data
         }
       }
     });
+
   }
 }
 
